b06cdff8e37d3d13e13a98cc72f32253370c2e73
Anthony Weyer <antweyer@iu.edu>
2024-04-18 22:23:24 +0000

    web form can send to firestore

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,10 +1,45 @@
 let map;
+let markers = [];
+
+function loadGoogleMaps() {
+    fetch('/api/maps-api-key')
+        .then(response => response.text())
+        .then(apiKey => {
+            if (!apiKey) {
+                console.error('Failed to load the Google Maps API key.');
+                return;
+            }
+
+            // Create a new script element for Google Maps API
+            const script = document.createElement('script');
+            script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+            script.async = true;
+            script.defer = true;
+            document.head.appendChild(script);
+        })
+        .catch(err => {
+            console.error('Error loading the Google Maps script:', err);
+        });
+}
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.8781, lng: -87.6298 },
-    zoom: 12,
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 41.8781, lng: -87.6298 },
+        zoom: 12,
+    });
+
+// data changes as you zoom in
+map.addListener('zoom_changed',()=>{
+
+let zoom = map.getZoom();
+markers.forEach(marker =>{
+  if(zoom>15){
+    marker.setMap(map);
+  }else{
+    marker.setMap(null);
+  }
+});
+});
 }
 
 function searchLocation() {
@@ -22,4 +57,9 @@ function searchLocation() {
   })
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadGoogleMaps(); 
+    document.getElementById('submit').addEventListener('click', searchLocation);
+
+});
